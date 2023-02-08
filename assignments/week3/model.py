4590f67dd43b0002bfbb8aa26a3fd02cf9513bbe
Yash Nakadi <yashnakadi@gmail.com>
2023-02-07 21:03:19 -0500

    changes

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -1,15 +1,17 @@
 from typing import Callable
+import torch
 
 
-class MLP:
+class MLP(torch.nn.Module):
     def __init__(
-        self,
-        input_size: int,
-        hidden_size: int,
-        num_classes: int,
-        hidden_count: int = 1,
-        activation: Callable = torch.nn.ReLU,
-        initializer: Callable = torch.nn.init.ones_,
+
+            self,
+            input_size: int,
+            hidden_size: int,
+            num_classes: int,
+            hidden_count: int = 1,
+            activation: Callable = torch.nn.ReLU(),
+            initializer: Callable = torch.nn.init.ones_,
     ) -> None:
         """
         Initialize the MLP.
@@ -21,7 +23,19 @@ class MLP:
             activation: The activation function to use in the hidden layer.
             initializer: The initializer to use for the weights.
         """
-        ...
+        super(MLP, self).__init__()
+        self.actv = activation
+        self.layers = torch.nn.ModuleList()
+        print('hi')
+        for i in range(hidden_count):
+            next_num_inputs = hidden_size
+            self.layers += [torch.nn.Linear(input_size, next_num_inputs)]
+            input_size = next_num_inputs
+
+            # Create final layer
+        print(self.layers)
+        print('hello')
+        self.out = torch.nn.Linear(input_size, num_classes)
 
     def forward(self, x):
         """
@@ -33,4 +47,9 @@ class MLP:
         Returns:
             The output of the network.
         """
-        ...
+        x = x.reshape(x.shape[0], -1)
+        for layer in self.layers:
+            x = self.actv(layer(x))
+
+        # Get activations of each layer
+        return self.out(x)
