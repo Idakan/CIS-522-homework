11ee7fe3d5c92cb42d47baf9e940de0ba0f8e58e
Yash Nakadi <yashnakadi@gmail.com>
2023-02-18 15:21:03 -0500

    new code

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -6,21 +6,24 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 
 
 class CONFIG:
-
     batch_size = 64
     num_epochs = 2
     initial_learning_rate = 0.001
     initial_weight_decay = 0
+    gamma = 0.1
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
+        "gamma": 0.999
     }
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(
-        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay,
+        model.parameters(),
+        lr=CONFIG.initial_learning_rate,
+        weight_decay=CONFIG.initial_weight_decay,
     )
 
     transforms = Compose(
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -4,6 +4,32 @@ from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CustomLRScheduler(_LRScheduler):
+    def __init__(self, optimizer, gamma, last_epoch=-1, verbose=False):
+        """
+        Create a new scheduler.
+
+        Note to students: You can change the arguments to this constructor,
+        if you need to add new parameters.
+
+        """
+        # ... Your Code Here ...
+        super(CustomLRScheduler, self).__init__(optimizer, last_epoch, verbose)
+        self.gamma = gamma
+
+    def get_lr(self) -> List[float]:
+        # Note to students: You CANNOT change the arguments or return type of
+        # this function (because it is called internally by Torch)
+
+        # ... Your Code Here ...
+        # Here's our dumb baseline implementation:
+        if self.last_epoch == 0:
+            return [x["lr"] for x in self.optimizer.param_groups]
+
+        else:
+            return [x["lr"] * self.gamma for x in self.optimizer.param_groups]
+
+
+class CustomLRScheduler2(_LRScheduler):
     def __init__(self, optimizer, last_epoch=-1):
         """
         Create a new scheduler.
@@ -13,7 +39,9 @@ class CustomLRScheduler(_LRScheduler):
 
         """
         # ... Your Code Here ...
-        super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
+        super(CustomLRScheduler2, self).__init__(optimizer, last_epoch)
+        self.optimizer = optimizer
+        self.last_epoch = last_epoch
 
     def get_lr(self) -> List[float]:
         # Note to students: You CANNOT change the arguments or return type of
@@ -21,4 +49,5 @@ class CustomLRScheduler(_LRScheduler):
 
         # ... Your Code Here ...
         # Here's our dumb baseline implementation:
+        self.base_lrs = self.optimizer.step()
         return [i for i in self.base_lrs]
